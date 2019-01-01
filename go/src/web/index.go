721b289ba7dadfefc94da83dc5fdf89ff7bd85a5
mogi86 <nobuyuki.mogi@gmail.com>
2019-01-01 12:40:47 +0900

    add simple http

--- go/src/web/index.go
+++ go/src/web/index.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+import "net/http"
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+    fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+	http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
