b8d108300637f0d7b0e9b55913e084ec8e945dd3
mogi86 <nobuyuki.mogi@gmail.com>
2019-01-01 11:43:07 +0900

    add array sample

---
+++ go/src/mygo/array/array.go
@@ -0,0 +1,8 @@
+package array
+
+import "fmt"
+
+func PrintArray() {
+    a := []string{"hoge", "fuga"}
+    fmt.Println(a)
+}
--- go/src/mygo/hello.go
+++ go/src/mygo/hello.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 import "github.com/fatih/color"
+import "mygo/array"
 
 func main() {
 	fmt.Println("Hello, 世界")
 	color.Red("赤")
+
+	array.PrintArray()
 }
